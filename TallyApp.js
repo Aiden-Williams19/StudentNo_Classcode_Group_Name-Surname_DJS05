335579ad688dc45a3c19f414e84be77141279780
Aiden Williams <aidenwilliams336@gmail.com>
2024-11-14 23:42:50 +0200

    Add test scenarios for verifying counter state changes

--- TallyApp.js
+++ TallyApp.js
@@ -47,3 +47,20 @@ function counterReducer(state = { count: 0 }, action) {
   // Log state changes to console
   store.subscribe(() => console.log('State:', store.getState()));
 
+
+  // Scenario 1: Initial State Verification
+console.log("SCENARIO 1: Initial State Verification");
+console.log("Initial state:", store.getState());
+
+// Scenario 2: Incrementing the Counter
+console.log("\nSCENARIO 2: Incrementing the Counter");
+store.dispatch({ type: 'ADD' });
+store.dispatch({ type: 'ADD' });
+
+// Scenario 3: Decrementing the Counter
+console.log("\nSCENARIO 3: Decrementing the Counter");
+store.dispatch({ type: 'SUBTRACT' });
+
+// Scenario 4: Resetting the Counter
+console.log("\nSCENARIO 4: Resetting the Counter");
+store.dispatch({ type: 'RESET' });
